7cf5bfc58a28ff7b2c73dbaefd7bee1e332ce711
asdf123dd <vedanth.ramji@outlook.com>
2021-10-05 12:54:54 +0530

    updated cancerpagedata

--- src/components/cancerPage/CancerPageData.jsx
+++ src/components/cancerPage/CancerPageData.jsx
@@ -6,7 +6,11 @@ export const CancerPageData = {
         survival_curves_link: 'https://seer.cancer.gov/explorer/application.html?site=92&data_type=4&graph_type=6&compareBy=age_range&chk_age_range_16=16&chk_age_range_62=62&chk_age_range_122=122&chk_age_range_160=160&chk_age_range_166=166&sex=1&race=1&hdn_stage=101&advopt_precision=1&advopt_show_ci=on&advopt_display=2#label_graphArea',
         search_link: 'https://cse.google.com/cse?cx=005505425243181267405:xmmlkfi5txg&q=Acute%20Lymphocytic%20Leukemia'
     },
-    acute_monocytic_leukemia: {},
+    acute_monocytic_leukemia: {
+        name: 'Acute Monocytic Leukemia',
+        approved_drugs_link: 'https://www.cancer.gov/about-cancer/treatment/drugs/leukemia',
+        clinical_trials_link: ''
+    },
     acute_myeloid_leukemia: {},
     chronic_lyphocytic_leukemia: {},
     chronic_myeloid_leukemia: {},
