5a2c4cb77b671edef9605b4f1ab211389388ee20
asdf123dd <vedanth.ramji@outlook.com>
2021-10-05 12:51:31 +0530

    adding cancer page data

---
+++ src/components/cancerPage/CancerPageData.jsx
@@ -0,0 +1,16 @@
+export const CancerPageData = {
+    acute_lymphocytic_leukemia: {
+        name: 'Acute Lymphocytic Leukemia',
+        approved_drugs_link: 'https://www.cancer.gov/about-cancer/treatment/drugs/leukemia#1',
+        clinical_trials_link: 'https://www.clinicaltrials.gov/ct2/results?cond=Acute+Lymphocytic+Leukemia&term=&type=&rslt=&recrs=a&recrs=f&recrs=e&recrs=c&age_v=&gndr=&intr=&titles=&outc=&spons=&lead=&id=&cntry=&state=&city=&dist=&locn=&rsub=&strd_s=&strd_e=&prcd_s=&prcd_e=&sfpd_s=&sfpd_e=&rfpd_s=&rfpd_e=&lupd_s=&lupd_e=&sort=',
+        survival_curves_link: 'https://seer.cancer.gov/explorer/application.html?site=92&data_type=4&graph_type=6&compareBy=age_range&chk_age_range_16=16&chk_age_range_62=62&chk_age_range_122=122&chk_age_range_160=160&chk_age_range_166=166&sex=1&race=1&hdn_stage=101&advopt_precision=1&advopt_show_ci=on&advopt_display=2#label_graphArea',
+        search_link: 'https://cse.google.com/cse?cx=005505425243181267405:xmmlkfi5txg&q=Acute%20Lymphocytic%20Leukemia'
+    },
+    acute_monocytic_leukemia: {},
+    acute_myeloid_leukemia: {},
+    chronic_lyphocytic_leukemia: {},
+    chronic_myeloid_leukemia: {},
+    liver_lymphatic_bile_and_duct: {},
+    lungs: {},
+    pancreas: {}
+}
