0aab0288402d8138693f764e723f748fcd9a05fd
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2021-11-01 13:22:14 +0530

    here we goooooooooooooooooooo started internationalizing homepage

---
+++ src/i18n/en_us.jsx
@@ -0,0 +1,11 @@
+export const en_us = {
+	"what am I doing": "I don't know",
+	"homePage": {
+		"header": "A quick reference guide for different types of cancers",
+		"searchBoxPlaceholder": "Search for a cancer",
+		"cancerListAccordionTitle": "Cancer List"
+	},
+	"cancers": {
+		"should this even be internationalized?": "I don't know"
+	}
+};
--- src/pages/generalPages/HomePage.jsx
+++ src/pages/generalPages/HomePage.jsx
@@ -1,11 +1,13 @@
 import CancerMenu from '../../components/cancerMenu/CancerMenu';
 import SubHeader from '../../components/SubHeader';
 import '../../style.css';
+import {en_us} from "../../i18n/en_us.jsx";
 
+const lang = en_us;//Note, the lang object should probably be passed as an argument to the function so it can be easily switched.
 export default function HomePage() {
     return (
         <div className='homepage'>
-            <SubHeader text="A quick reference guide for different types of cancers" />
+            <SubHeader text={lang.homePage.header} />
             <CancerMenu />
         </div>
     )
