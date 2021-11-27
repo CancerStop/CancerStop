41ee8b3734795820de32f141d0cb0d8606a2dd23
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2021-11-27 17:31:49 +0530

    🤔

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,9 +13,9 @@ export default function Footer() {
                 © 2021 Queromatics. All Rights Reserved | 
                 Developed by
                 <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/vedanth-ramji-191530215/'> Vedanth Ramji</a>,
-                <a target="_blank" rel="noreferrer" href="https://github.com/BalaM314">Balaadithya Muralitharan</a>,
+                <a target="_blank" rel="noreferrer" href="https://github.com/BalaM314"> Balaadithya Muralitharan</a>,
                 <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/ganeshramjanakiraman/'> Ganesh Ram</a> and
-                <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/natarajanganesan/'> Dr. Natarajan Ganesan</a>
+                <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/natarajanganesan/'> Dr. Natarajan Ganesan</a>.
             </p>
         </div>
     )
