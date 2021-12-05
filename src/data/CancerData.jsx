883de63ce21977f8472e099ec512681e38b71594
asdf123dd <vedanth.ramji@outlook.com>
2021-12-05 05:54:05 +0530

    changes to data

--- src/data/CancerData.jsx
+++ src/data/CancerData.jsx
@@ -33,12 +33,6 @@ export const CancerData = {
         clinical_trials_link: 'https://www.clinicaltrials.gov/ct2/results?cond=Chronic+Myeloid+Leukemia&term=&type=&rslt=&recrs=b&recrs=a&recrs=f&recrs=e&recrs=c&age_v=&gndr=&intr=&titles=&outc=&spons=&lead=&id=&cntry=&state=&city=&dist=&locn=&rsub=&strd_s=&strd_e=&prcd_s=&prcd_e=&sfpd_s=&sfpd_e=&rfpd_s=&rfpd_e=&lupd_s=&lupd_e=&sort=',
         survival_curves_link: 'https://seer.cancer.gov/explorer/application.html?site=97&data_type=4&graph_type=6&compareBy=age_range&chk_age_range_16=16&chk_age_range_62=62&chk_age_range_122=122&chk_age_range_160=160&chk_age_range_166=166&sex=1&race=1&hdn_stage=101&advopt_precision=1&advopt_show_ci=on&advopt_display=2#label_graphArea',
         search_link: 'https://cse.google.com/cse?cx=005505425243181267405:xmmlkfi5txg&q=Chronic%20Myeloid%20Leukemia',
-        survival_curves_data: {
-            age_less_than_20: [],
-            age_between_20_and_49: [],
-            age_between_50_and_64: [],
-            age_above_75: []
-        }
     },
     liver_lymphatic_bile_and_duct: {
         name: 'Liver Lymphatic Bile and Duct',
