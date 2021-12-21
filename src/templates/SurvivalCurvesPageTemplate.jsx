c37ee439adb1bde1e5cc353d2d25c3329da94da9
asdf123dd <vedanth.ramji@outlook.com>
2021-12-21 13:34:50 +0530

    started off with survival curves page template

---
+++ src/templates/SurvivalCurvesPageTemplate.jsx
@@ -0,0 +1,16 @@
+import LineGraph from '../components/LineGraph';
+import SubHeader from '../components/SubHeader';
+
+export default function SurvivalCurvesPageTemplate(xAxisLabels, dataSets, cancerName) {
+    return (
+        <div className='survivalCurvesPageTemplate'>
+            <SubHeader text={cancerName} />
+
+            <LineGraph
+                xAxisLabels={xAxisLabels}
+                dataSets={dataSets}
+                cancerName={cancerName}
+            />
+        </div>
+    )
+}
