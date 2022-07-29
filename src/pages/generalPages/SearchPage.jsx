045cff213fc081b323c0d9f945e27f853ed0b0ba
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-07-29 17:43:04 +0530

    Start working on internalizing the search page

--- src/pages/generalPages/SearchPage.jsx
+++ src/pages/generalPages/SearchPage.jsx
@@ -5,8 +5,16 @@ export default function SearchPage() {
     return (
         <div className='searchPage'>
             <SubHeader text='Search' />
-            <div className='searchPageContent'>
-                [WIP]
+            {(() => {
+                /* I'm not sure if this is the best way to do this */
+                const script = document.createElement("script");
+                script.src = "https://cse.google.com/cse.js?cx=004715292727045167679:c87ei5ji6we";
+                script.async = true;
+            
+                document.body.appendChild(script);
+            })()}
+            <div className='gcse-search'>
+                Loading...
             </div>
         </div>
     )
