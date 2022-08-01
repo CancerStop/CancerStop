56b01fc0a6d1b1d37f4731cbce6dff0d2baccd63
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-08-01 13:26:02 +0530

    Add subtitle on home page

--- src/pages/generalPages/HomePage.jsx
+++ src/pages/generalPages/HomePage.jsx
@@ -5,6 +5,10 @@ export default function HomePage() {
     return (
         <div className='homepage'>
             <SubHeader text='A quick reference guide to different types of cancers' />
+            <div style={{textAlign: "center", color: "gray", marginTop: "5px"}}>
+                From therapies to genomics
+                - See what the public databases and resources say
+            </div>
             <CancerMenu />
         </div>
     )
