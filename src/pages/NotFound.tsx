b78da2696caa6c5ef833554b7d9b3b9f9ef2ef45
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-04-07 12:35:53 +0530

    Fix not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
 	return (
 		<div className="notFoundPage page">
 			<SubHeader text="404 Not Found" />
-			<a href="/">Return to the home page</a>
+			<a href="/" id="returnLink">Return to the home page</a>
 		</div>
 	);
 }
