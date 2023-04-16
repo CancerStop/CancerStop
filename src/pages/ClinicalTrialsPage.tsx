006c28265a7c6aeb4aee257f8dd1f18a2067654d
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-04-16 11:57:37 +0530

    Rewrite clinical trials using mui-x DataGrid

--- src/pages/ClinicalTrialsPage.tsx
+++ src/pages/ClinicalTrialsPage.tsx
@@ -1,92 +1,131 @@
 import SubHeader from '../components/SubHeader';
 import '../styles/pageStyles/ClinicalTrialsStyles.css';
 import { CTColumn, CTStudy, StudiesResponse, findStudies } from '../util/api';
-import { useState, ChangeEvent, useEffect } from 'react';
-import { Table, TableContainer, TableRow, Button, TableBody, TableHead, TableCell } from '@material-ui/core';
+import { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+import { DataGrid, GridColDef, GridPaginationModel } from '@mui/x-data-grid';
 
-const defaultColumns:CTColumn[] = ["ID", "Title", "Condition", "Status"];
-const resultsPerPage = 20;
-const columnTooltips: Partial<Record<CTColumn, string>> = {
-	ID: "The NCT ID for this study.",
-};
 const searchStatusName = {
 	"idle": "Search",
 	"searching": "Searching...",
 	"errored": "Search failed"
 };
 
-type StudySort = [property:CTColumn | "default", direction: -1 | 1];
-
-function compareProps<T>(a:T, b:T):number {
-	if(Array.isArray(a) && Array.isArray(b)){
-		let i = 0;
-		while(i < a.length && i < b.length){
-			const diff = compareProps(a.at(i), b.at(i));
-			if(diff !== 0) return diff;
-			i ++;
-		}
-		if(a.length !== b.length) return a.length - b.length;
-		return 0;
-	} else if(typeof a == "string" && typeof b == "string"){
-		let i = 0;
-		while(i < a.length && i < b.length){
-			const diff = a.codePointAt(i)! - b.codePointAt(i)!;
-			if(diff !== 0) return diff;
-			i ++;
-		}
-		if(a.length !== b.length) return a.length - b.length;
-		return 0;
-	} else throw new Error(`Cannot compare object ${a} (type ${typeof a})`);
-}
-
-function compareStudies(a:CTStudy, b:CTStudy, sort:StudySort):number {
-	if(sort[0] === "default") return 0;
-	return compareProps(a[sort[0]], b[sort[0]]) * sort[1];
-}
+const dataGridColumns: GridColDef[] = [
+	{
+		field: "ID",
+		headerName: "NCT ID",
+		description: "The NCT ID for this study.",
+		minWidth: 140,
+		flex: 0.15,
+	},
+	{
+		field: "Title",
+		headerName: "Title",
+		renderCell(params) {
+			if(typeof params.value === "string"){
+				return <a href={params.row.url} target="_blank" rel="noreferrer">{params.value}</a>;
+			} else return `TypeError: ${typeof params.value}`;
+		},
+		minWidth: 200,
+		flex: 1,
+	},
+	{
+		field: "StudyType",
+		headerName: "Type",
+		description: "Interventional studies study the effect of an intervention, such as administering a new medication. Observational studies measure effects without attempting to change the outcome (no treatments are given).",
+		minWidth: 130,
+		flex: 0.1,
+	},
+	{
+		field: "Status",
+		headerName: "Status",
+		minWidth: 120,
+		flex: 0.2
+	},
+	{
+		field: "HasResults",
+		headerName: "Results",
+		minWidth: 100,
+		flex: 0.07,
+		renderCell(params) {
+			if(typeof params.value === "boolean"){
+				return params.value
+					? <a href={`https://www.clinicaltrials.gov/ct2/show/results/NCT02336282`} target="_blank" rel="noreferrer">Yes</a>
+					: "No";
+			} else return `TypeError: ${typeof params.value}`;
+		},
+	},
+	{
+		field: "Condition",
+		headerName: "Condition",
+		minWidth: 150,
+		flex: 0.4,
+	},
+	{
+		field: "Locations",
+		headerName: "Locations",
+		description: "List of all locations involved in this study.",
+		renderCell(params) {
+			if(Array.isArray(params.value)){
+				return params.value.join(", ");
+			} else return `TypeError: ${typeof params.value}`;
+		},
+		minWidth: 200,
+		flex: 1.1
+	},
+	{
+		field: "Phase",
+		headerName: "Phase",
+		description: "A Phase 1 study is performed with 20 to 80 participants to determine safe dosage. Phase 2 studies involve hundreds of participants, and check for long term side effects. Phase 3 and 4 studies involve thousands of participants.",
+		renderCell(params) {
+			if(Array.isArray(params.value)){
+				if(params.value.length && params.value.every(p => /Phase \d/.test(p)))
+					return `Phase ${params.value.map(p => p.split(" ")[1]).join(", ")}`;
+				else if(params.value.length === 0 || params.value[0] === "Not Applicable"){
+					return "N/A";
+				} return params.value.join(", ");
+			} else return `TypeError: ${typeof params.value}`;
+		},
+		minWidth: 100,
+		flex: 0.1
+	},
+];
 
 export default function ClinicalTrialsPage() {
 
-	const [columns, setColumns] = useState<CTColumn[]>(defaultColumns);
 	const [response, setResponse] = useState<StudiesResponse | null>(null);
 	const [searchExpr, setSearchExpr] = useState<string>(
 		new URLSearchParams(window.location.search).get("cond")?.split("+").join(" ") ?? ""
 	);
-	const [page, setPage] = useState(1);
+	const [paginationModel, setPaginationModel] = useState({
+		pageSize: 25,
+		page: 0,
+	});
 	const [searchStatus, setSearchStatus] = useState<"idle" | "searching" | "errored">("idle");
-	const [sort, setSort] = useState<StudySort>(["default", 1]);
 
-	const fetchData = (p = page) => findStudies(searchExpr, (p - 1) * resultsPerPage + 1, p * resultsPerPage);
-	const numberInputUpdated = (e:ChangeEvent<HTMLInputElement>) => {
-		const val = + e.target.value;
-		const maxPage = Math.ceil((response?.totalStudiesAvailable ?? 1) / resultsPerPage);
-		if(isNaN(val) || val <= 0){
-			e.target.value = "1";
-			setPage(1);
-		} else if(val > maxPage){
-			e.target.value = maxPage.toString();
-			setPage(maxPage);
-		} else {
-			setPage(val);
-		}
-	};
-	const search = () => {
+	const fetchData = (page = paginationModel.page, resultsPerPage = paginationModel.pageSize) => findStudies(searchExpr, (page) * resultsPerPage + 1, (page + 1) * resultsPerPage);
+	const search = (model = paginationModel) => {
 		setSearchStatus("searching");
-		fetchData().then(data => {
-			if(page > Math.ceil(data.totalStudiesAvailable / resultsPerPage) && data.studies.length === 0){
+		fetchData(model.page, model.pageSize).then(data => {
+			if(model.page > Math.ceil(data.totalStudiesAvailable / model.pageSize) && data.studies.length === 0){
 				//If no studies were returned and page is more than the new max page, refetch
-				setPage(1);
-				fetchData(1).then(setResponse).catch(e => setSearchStatus("errored"));
-				setSearchStatus("idle");
+				setPaginationModel({
+					page: 0, pageSize: model.pageSize
+				});
+				fetchData(0).then(d => {
+					setResponse(d);
+					setSearchStatus("idle");
+				}).catch(e => setSearchStatus("errored"));
 			} else {
 				setResponse(data);
 				setSearchStatus("idle");
 			}
 		}).catch(e => setSearchStatus("errored"));
 	}
-
-	function format(obj:string | string[]){
-		if(Array.isArray(obj)) return obj.join(", ");
-		else return obj;
+	const reloadWithPaginationModel = (model:GridPaginationModel) => {
+		setPaginationModel(model);
+		search(model);
 	}
 
 	useEffect(() => {
@@ -105,43 +144,26 @@ export default function ClinicalTrialsPage() {
 					onChange={(e) => setSearchExpr(e.target.value)}
 					id="searchInput"
 				/>
-				<span id="pageSelector">
-					Page <input value={page} type="number" onChange={numberInputUpdated} className="numberInput"/> of {Math.ceil((response?.totalStudiesAvailable ?? 1) / resultsPerPage)}
-				</span>
 				<Button
-					onClick={search}
+					onClick={() => search()}
 					variant="contained" id="searchButton"
 					disabled={searchStatus === "searching"}
 				>
 					{searchStatusName[searchStatus]}
 				</Button>
-				<Button onClick={() => setColumns(prompt("Columns (comma separated):", defaultColumns.join(","))?.split(/, ?/) as CTColumn[] ?? defaultColumns)}>TEMP:set columns</Button>
-				<Button onClick={() => setSort(prompt("Column, sort order", "ID,1")?.split(/, ?/) as StudySort ?? ["ID", 1])}>TEMP:set sort</Button>
 			</span>
-			<TableContainer>
-				<Table stickyHeader>
-					<TableHead>
-						<TableRow>
-							{columns.map(name =>
-								<TableCell title={columnTooltips[name]} key={name}>{name}</TableCell>
-							)}
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{response && response.studies.sort((a, b) => compareStudies(a, b, sort)).map(s =>
-							<TableRow key={s.ID} >
-								{columns.map(name =>
-									<TableCell>{
-										name === "Title"
-											? <a href={s.url} target="_blank" rel="noreferrer">{s.Title}</a>
-											: format(s[name as CTColumn])
-									}</TableCell>
-								)}
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</TableContainer>
+			{response ? <DataGrid
+				columns={dataGridColumns}
+				rows={response.studies}
+				getRowId={row => row.ID}
+				autoHeight={true}
+				getRowHeight={() => "auto"}
+				paginationMode="server"
+				rowCount={response.totalStudiesAvailable}
+				paginationModel={paginationModel}
+				onPaginationModelChange={reloadWithPaginationModel}
+				rowSelection={false}
+			/> : searchStatus === "searching" ? "Loading..." : ""}
 		</div>
 	);
 }
--- src/util/api.ts
+++ src/util/api.ts
@@ -1,6 +1,6 @@
 
 type CTStatus = "Unknown status" | "Recruiting" | "Completed" | "Not yet recruiting" | "Withdrawn" | "Terminated" | "Active, not recruiting" | "Enrolling by invitation";
-
+type CTStudyType = "Interventional" | "Observational" | "Expanded Access";
 export interface CTStudy {
 	ID: string;
 	Condition: string;
@@ -8,6 +8,9 @@ export interface CTStudy {
 	Status: CTStatus;
 	url: string;
 	Locations: string[];
+	Phase: string[];
+	StudyType: CTStudyType;
+	HasResults: boolean;
 }
 
 export type CTColumn = "ID" | "Condition" | "Title" | "Status" | "Locations";
@@ -31,6 +34,9 @@ interface CTStudyFieldsResponse {
 			BriefTitle: string[];
 			OverallStatus: string[];
 			LocationFacility: string[];
+			Phase: string[];
+			StudyType: string[];
+			ResultsFirstSubmitDate: string[] | [];
 		}>;
 	}
 }
@@ -41,7 +47,10 @@ export type StudiesResponse = {
 };
 
 
-export async function findStudies(searchExpr:string, minRank:number = 1, maxRank:number = 20, fields = ["NCTId", "Condition", "BriefTitle", "OverallStatus", "LocationFacility"]):Promise<StudiesResponse> {
+export async function findStudies(
+	searchExpr:string, minRank:number = 1, maxRank:number = 20,
+	fields = ["NCTId", "Condition", "BriefTitle", "OverallStatus", "LocationFacility", "Phase", "StudyType", "ResultsFirstSubmitDate"]
+):Promise<StudiesResponse> {
 	const url = `https://www.clinicaltrials.gov/api/query/study_fields?` + new URLSearchParams({
 		expr: searchExpr,
 		fields: fields.join(","),
@@ -50,6 +59,7 @@ export async function findStudies(searchExpr:string, minRank:number = 1, maxRank
 		max_rnk: maxRank.toString(),
 	}).toString();
 	const data = await (await fetch(url)).json() as CTStudyFieldsResponse;
+	console.debug(`Fetched url ${url}, has results: ${data.StudyFieldsResponse.StudyFields![0].ResultsFirstSubmitDate}`);
 	return {
 		studies: data.StudyFieldsResponse.StudyFields?.map(obj => ({
 			ID: obj.NCTId[0],
@@ -58,6 +68,10 @@ export async function findStudies(searchExpr:string, minRank:number = 1, maxRank
 			Title: obj.BriefTitle[0],
 			Locations: obj.LocationFacility,
 			url: `https://www.clinicaltrials.gov/ct2/show/${obj.NCTId[0]}`,
+			Phase: obj.Phase,
+			StudyType: obj.StudyType[0] as CTStudyType,
+			HasResults: obj.ResultsFirstSubmitDate.length > 0,
+			//If there is a results submit date then the study has results, otherwise it doesn't
 		})) ?? [],
 		totalStudiesAvailable: data.StudyFieldsResponse.NStudiesFound
 	};
