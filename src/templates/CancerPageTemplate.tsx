c04bd5d6fd28cbd94d3546e8c24cff2c2547bcc4
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-06-05 11:04:53 +0530

    Remove redundant code

--- src/templates/CancerPageTemplate.tsx
+++ src/templates/CancerPageTemplate.tsx
@@ -9,85 +9,43 @@ import survivalCurvesImage from '../images/cancerPageCardImages/survivalCurvesIm
 import { CancerData } from '../data/CancerData';
 
 export default function CancerPageTemplate(data:CancerData) {
-	if (data.internalized_survival_curves) {
-		return () => (
-			<div className="cancerPageTemplate">
-				<SubHeader>{data.name}</SubHeader>
-				<div className="cancerDescription">
-					{/*data.description*/} <a href={data.information_link} target='_blank' rel="noreferrer">Learn more</a>
-				</div>
-				<div className="cancerPageTemplate_actionButtons">
-					<CancerPageCard
-						title="Approved Drugs"
-						link={data.approved_drugs_link}
-						imagePath={approvedDrugsImage}
-					/>
-	
-					<CancerPageCard
-						title="Clinical Trials"
-						link={data.clinical_trials_link}
-						imagePath={clinicalTrialsImage}
-					/>
-	
-					<CancerPageCard
-						title="Genes & More"
-						link="/genes-and-more"
-						imagePath={genesAndMoreImage}
-					/>
-	
-					<CancerPageCard
-						title="PresciQure"
-						link={data.search_link}
-						imagePath={searchImage}
-					/>
-	
-					<CancerPageCard
-						title="Survival Curves"
-						link={data.survival_curves_link_internal}
-						imagePath={survivalCurvesImage}
-					/>
-				</div>
+	return () => (
+		<div className="cancerPageTemplate">
+			<SubHeader>{data.name}</SubHeader>
+			<div className="cancerDescription">
+				{/*data.description*/} <a href={data.information_link} target='_blank' rel="noreferrer">Learn more</a>
 			</div>
-		);
-	} else {
-		return () => (
-			<div className="cancerPageTemplate">
-				<SubHeader>{data.name}</SubHeader>
-				<div className="cancerDescription">
-					{/*data.description*/} <a href={data.information_link} target='_blank' rel="noreferrer">Learn more</a>
-				</div>
-				<div className="cancerPageTemplate_actionButtons">
-					<CancerPageCard
-						title="Approved Drugs"
-						link={data.approved_drugs_link}
-						imagePath={approvedDrugsImage}
-					/>
-	
-					<CancerPageCard
-						title="Clinical Trials"
-						link={data.clinical_trials_link}
-						imagePath={clinicalTrialsImage}
-					/>
-	
-					<CancerPageCard
-						title="Genes & More"
-						link="/genes-and-more"
-						imagePath={genesAndMoreImage}
-					/>
-	
-					<CancerPageCard
-						title="PresciQure"
-						link={data.search_link}
-						imagePath={searchImage}
-					/>
-	
-					<CancerPageCard
-						title="Survival Curves"
-						link={data.survival_curves_link_external}
-						imagePath={survivalCurvesImage}
-					/>
-				</div>
+			<div className="cancerPageTemplate_actionButtons">
+				<CancerPageCard
+					title="Approved Drugs"
+					link={data.approved_drugs_link}
+					imagePath={approvedDrugsImage}
+				/>
+
+				<CancerPageCard
+					title="Clinical Trials"
+					link={data.clinical_trials_link}
+					imagePath={clinicalTrialsImage}
+				/>
+
+				<CancerPageCard
+					title="Genes & More"
+					link="/genes-and-more"
+					imagePath={genesAndMoreImage}
+				/>
+
+				<CancerPageCard
+					title="PresciQure"
+					link={data.search_link}
+					imagePath={searchImage}
+				/>
+
+				<CancerPageCard
+					title="Survival Curves"
+					link={data.internalized_survival_curves ? data.survival_curves_link_internal : data.survival_curves_link_external}
+					imagePath={survivalCurvesImage}
+				/>
 			</div>
-		);
-	}
+		</div>
+	);
 }
