15352c5669f096cd56a58e6da3271b136260ca0e
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-09-02 21:56:52 +0530

    Fix typedefs for Object.entries, etc

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+interface ObjectConstructor {
+	/**
+	 * Returns an array of key/values of the enumerable properties of an object
+	 * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+	 */
+	keys<K extends PropertyKey>(o: Record<K, unknown>):K[];
+	entries<K extends PropertyKey, T>(o: { [_ in K]: T } | ArrayLike<T>): [K, T][];
+	fromEntries<K extends PropertyKey, T>(o: Iterable<readonly [K, T]>): Record<K, T>;
+}
+
